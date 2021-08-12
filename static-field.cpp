245955c8552a5e6edf0c74dd0d4b5abdbb5ef7db
kanon 122 <kanon121chakma@gmail.com>
2021-08-12 09:19:11 +0600

    add static-filed

--- static-field.cpp
+++ static-field.cpp
@@ -20,11 +20,12 @@ public:
         cout << "Account rate of interest is " << rateOfInterest << endl;
     }
 };
-
+float Account::rateOfInterest = 4.5;
 int main()
 {
-    float Account::rateOfInterest = 4.5;
     Account a1(34, "Jonota Bank");
+    Account a2(45, "Sonali Bank");
     a1.Display();
+    a2.Display();
     return 0;
 }
